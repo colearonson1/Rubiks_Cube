503db1f3f90908e7b6cda1333828fc1c6440279a
colearonson1 <colearonsonw@gmail.com>
2017-07-16 16:05:32 -0500

    adding classes and methods

---
+++ src/rubiks_cube/Turns.java
@@ -0,0 +1,68 @@
+package rubiks_cube;
+
+/**
+ *
+ * @author Cole Aronson
+ * Function: houses all of the turn function (ex:R,R',L,L')
+ */
+public class Turns {
+    public static Cube R(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube Rv(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube L(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube Lv(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube U(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube Uv(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube B(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube Bv(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube D(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube Dv(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube F(Cube x)
+    {
+        return x;
+    }
+    
+    public static Cube Fv(Cube x)
+    {
+        return x;
+    }
+}
